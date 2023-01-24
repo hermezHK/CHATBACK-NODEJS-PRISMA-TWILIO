19fa722540bab0521cf6f6885af96a7528c7fdb3
Hermez <angelo_stk06@hotmail.com>
2023-01-24 03:40:58 -0500

    agregue las routes y configure el puerto

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { UserRouter } from "../components";
+
+const routes = [["/user", UserRouter]];
+
+const router = (app) => {
+  routes.forEach(([path, controller]) =>
+    app.use(`/api/v1/${path}`, controller)
+  );
+};
+
+export default router;
